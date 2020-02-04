03e8e9c0740afbb2f05a8832fc00abbe8f451cb3
chiba-8610 <mshlt@icloud.com>
2020-02-04 15:31:41 +0900

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+with open('input.txt') as f:
+    l=f.readlines()
+    
+m=int(l[-1].replace('\n',''))
+pair=[]
+for s in l[:-1]:
+    s=s.replace('\n','').split(':')
+    pair.append([int(s[0]),s[1]])
+pair=sorted(pair,key=lambda x:x[0])
+out=''
+for tmp in pair:
+    if m%tmp[0]==0:
+        out+=tmp[1]
+
+if out=='':
+    print(m)
+else:
+    print(out)
